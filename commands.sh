23663d68b76eaaa8dfa1475296028af5510c89c1
senha fathima <senhafathimaa@gmail.com>
2026-04-27 10:55:30 +0530

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# TCPDUMP
+sudo tcpdump -i wlan0 -nn
+sudo tcpdump -i wlan0 tcp
+sudo tcpdump -i wlan0 udp
+sudo tcpdump -i wlan0 port 53
+sudo tcpdump -i wlan0 port 80
+
+# TSHARK
+tshark -D
+sudo tshark -i wlan0
+sudo tshark -i wlan0 -Y dns
+sudo tshark -i wlan0 -Y http.request
+
+# BETTERCAP
+sudo bettercap -iface wlan0
+net.probe on
+arp.spoof on
+net.sniff on
